bd7d6871bc021dbf7e2fbaa6bf54bd0bdde92f3e
mabarroso <mabarroso@mabarroso.com>
2012-02-05 13:10:31 +0100

    Remove unnecessary tests

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '730853b3db36671a8c45b151e44c3653bf8d202d98f68acfe5a98fd51ce54cf818e6b9b7aaaf7374b677cf7ad10d27c1315d649bda9ec91f4e40cf99d83e5d9a'
